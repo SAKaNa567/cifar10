a8f3096fd1f3367da1f03053dfb1f2faa6cd65c8
naoto <s1240147@u-aizu.ac.jp>
2019-04-28 23:46:17 +0900

    dataaugmentation okay

--- cifar10_data_augmentation.py
+++ cifar10_data_augmentation.py
@@ -9,7 +9,8 @@ from keras.layers.core import Dense
 from keras.datasets import cifar10
 from keras.optimizers import RMSprop
 from keras.callbacks import TensorBoard, ModelCheckpoint
-
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 
 def network(input_shape, num_classes):
     model = Sequential()
@@ -57,7 +58,7 @@ class Trainer():
         self._target.compile(loss=loss, optimizer=optimizer,metrics=["accuracy"])
         self.verbose = 1
         self.log_dir = os.path.join(os.path.dirname(__file__),"logdir")
-        self.model_file_name = "model_file.hdf5"
+        self.model_file_name = "cifar10_data_augmentation.hdf5"
 
     def train(self,x_train, y_train, batch_size, epochs, validation_split):
         if os.path.exists(self.log_dir):
@@ -65,15 +66,39 @@ class Trainer():
             shutil.rmtree(self.log_dir)
         os.mkdir(self.log_dir)
 
-        self._target.fit(x_train, y_train,
-                        batch_size=batch_size,
+        datagen = ImageDataGenerator(
+            featurewise_center=False,
+            samplewise_center=False,
+            featurewise_std_normalization=False,
+            samplewise_std_normalization=False,
+            zca_whitening=False,
+            rotation_range=0,
+            width_shift_range=0.1,
+            height_shift_range=0.1,
+            horizontal_flip=True,
+            vertical_flip=False)
+
+        datagen.fit(x_train)
+
+        indices = np.arange(x_train.shape[0])
+        np.random.shuffle(indices)
+        validation_size = int(x_train.shape[0] * validation_split)
+        x_train, x_valid = x_train[indices[:-validation_size],:], x_train[indices[-validation_size:], :]
+        y_train, y_valid = y_train[indices[:-validation_size],:], y_train[indices[-validation_size:], :]
+
+
+
+        self._target.fit_generator(
+                        datagen.flow(x_train, y_train,batch_size=batch_size),
+                        steps_per_epoch=x_train.shape[0]//batch_size,
                         epochs=epochs,
-                        validation_split=validation_split,
+                        validation_data = (x_valid,y_valid),
                         callbacks=[
                         TensorBoard(log_dir=self.log_dir),
                         ModelCheckpoint(os.path.join(self.log_dir,self.model_file_name),save_best_only=True)
                         ],
-                        verbose=self.verbose
+                        verbose=self.verbose,
+                        workers=4
                         )
 
 dataset = CIFAR10Dataset()
